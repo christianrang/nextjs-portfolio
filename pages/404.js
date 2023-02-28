1df420fa9e6de88ce9905ef59d5954c4d4c529f1
Christian Rang <44748386+misclikd@users.noreply.github.com>
2023-02-28 12:11:07 -0500

    chore: add title to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,11 @@
 import Layout from "@/components/layout/layout";
+import { useEffect } from 'react';
 
 const Custom404 = () => {
+    useEffect(() => {
+        document.title = "404 | Rang Corp";
+    }, []);
+
   return <Layout>Uhhhh... I think you are lost. This is 404 Avenue.</Layout>;
 };
 
