486fb83438be89a5e95d000704acfe845fccf5e8
Christian Rang <cj@rang-corp.com>
2023-09-29 12:04:07 -0400

    feat: add configuration for navbar projects

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,74 +4,99 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 type Project = {
-    name: string;
-    route: string;
-    enabled: boolean;
+  name: string;
+  route: string;
+  enabled: boolean;
 };
 
-type NavbarProps = {};
+// Thanks to getStaticProps not properly decoding the envars into types these
+// must be strings and must be converted to booleans
+type EnableRoutes = {
+    isSortEnabled: string;
+    isPathfinderEnabled: string;
+    isTicTacToeEnabled: string;
+}
 
-const Navbar = (_props: NavbarProps) => {
-    const [isShown, setIsShown] = useState(false);
-    const router = useRouter();
+type NavbarProps = {
+  enabledRoutes?: EnableRoutes,
+};
 
-    const projects: Project[] = [
-        {
-            name: "Sort",
-            route: "/sort",
-            enabled: false,
-        },
-        {
-            name: "Pathfind",
-            route: "/pathfind",
-            enabled: false,
-        },
-        {
-            name: "Tic Tac Toe",
-            route: "/tictactoe",
-            enabled: true,
-        },
-    ];
+const StringToBool = (input: string) => {
+  switch (input) {
+    case 'true':
+      return true
+    case 'false':
+      return false
+  }
 
-    return (
-        <>
-            <div className={styles.navbarcontainer}>
-                <div className={styles.navbaritem}>
-                    <Link href="/">Home</Link>
-                </div>
-                <div
-                    className={styles.navbaritem}
-                    onMouseEnter={() => setIsShown(true)}
-                    onMouseLeave={() => setIsShown(false)}
-                >
-                    Projects
-                    <>
-                        {isShown && (
-                            <>
-                                {projects.map(({ name, route, enabled}, index) => {
-                                if (enabled) {
-                                    return (
-                                    <div
-                                        key={index}
-                                        className={styles.dropdownstyle}
-                                        onClick={() => {
-                                            router.push(route);
-                                        }}
-                                    >
-                                        <Link key={index} href={route}>
-                                            {name}
-                                        </Link>
-                                    </div>
-                                    )
-                                }
-                                })}
-                            </>
-                        )}
-                    </>
-                </div>
-            </div>
-        </>
-    );
+  return false
+}
+
+const Navbar = (props: NavbarProps) => {
+  const [isShown, setIsShown] = useState(false);
+  const router = useRouter();
+
+  const isSortEnabled: boolean = props.enabledRoutes?.isSortEnabled === undefined ? false : StringToBool(props.enabledRoutes.isSortEnabled)
+  const isPathfindEnabled = props.enabledRoutes?.isPathfinderEnabled === undefined ? false : StringToBool(props.enabledRoutes.isPathfinderEnabled)
+  const isTicTacToeEnabled = props.enabledRoutes?.isTicTacToeEnabled === undefined ? false : StringToBool(props.enabledRoutes.isTicTacToeEnabled)
+
+  const projects: Project[] = [
+    {
+      name: "Sort",
+      route: "/sort",
+      enabled: isSortEnabled,
+    },
+    {
+      name: "Pathfind",
+      route: "/pathfind",
+      enabled: isPathfindEnabled,
+    },
+    {
+      name: "Tic Tac Toe",
+      route: "/tictactoe",
+      enabled: isTicTacToeEnabled,
+    },
+  ];
+
+  return (
+    <>
+      <div className={styles.navbarcontainer}>
+        <div className={styles.navbaritem}>
+          <Link href="/">Home</Link>
+        </div>
+        <div
+          className={styles.navbaritem}
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+        >
+          Projects
+          <>
+            {isShown && (
+              <>
+                {projects.map(({ name, route, enabled }, index) => {
+                  if (enabled) {
+                    return (
+                      <div
+                        key={index}
+                        className={styles.dropdownstyle}
+                        onClick={() => {
+                          router.push(route);
+                        }}
+                      >
+                        <Link key={index} href={route}>
+                          {name}
+                        </Link>
+                      </div>
+                    );
+                  }
+                })}
+              </>
+            )}
+          </>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export type { NavbarProps };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,48 @@
-import Layout from "../components/layout/layout";
+import Navbar, { NavbarProps } from "@/components/navbar/navbar";
+import Layout, { LayoutProps } from "../components/layout/layout";
 import { useEffect } from "react";
 
-export default function Home() {
-    useEffect(() => {
-        document.title = "Home | Rang Corp";
-    }, []);
+export async function getStaticProps() {
+  const isSortEnabled = process.env.NAVBAR_SORT_ENABLED;
+  const isPathfinderEnabled = process.env.NAVBAR_PATHFIND_ENABLED;
+  const isTicTacToeEnabled = process.env.NAVBAR_TICTACTOE_ENABLED;
 
-    return (
-        <>
-            <Layout>
-                <span>This site has a couple of demonstrations of random things. Use the Projects drop down to explore them all!</span>
-            </Layout>
-        </>
-    );
+  return {
+    props: {
+      navbarProps: {
+        enabledRoutes: {
+          isSortEnabled: isSortEnabled,
+          isPathfinderEnabled: isPathfinderEnabled,
+          isTicTacToeEnabled: isTicTacToeEnabled,
+        }
+      }
+    },
+  };
+}
+
+type HomeProps = {
+  navbarProps: NavbarProps,
+}
+
+export default function Home(
+      {
+        navbarProps
+      }: HomeProps
+) {
+  useEffect(() => {
+    document.title = "Home | Rang Corp";
+  }, []);
+
+  console.log(navbarProps);
+
+  return (
+    <>
+      <Layout navbarProps={navbarProps}>
+        <span>
+          This site has a couple of demonstrations of random things. Use the
+          Projects drop down to explore them all!
+        </span>
+      </Layout>
+    </>
+  );
 }
