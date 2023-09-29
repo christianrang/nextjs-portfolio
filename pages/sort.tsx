d424c5d59abdb7ac93e7af6e92d42106b2278ce4
Christian Rang <44748386+christianrang@users.noreply.github.com>
2023-09-29 12:07:37 -0400

    Envvar config for navbar (#9)
    * feat: add configuration for navbar projects
    
    * fix: correct the production ready paths in config
    
    ---------
    
    Co-authored-by: Christian Rang <cj@rang-corp.com>

--- pages/sort.tsx
+++ pages/sort.tsx
@@ -1,14 +1,37 @@
 import Sorter from '../components/sorter/sorter';
 import Layout from '../components/layout/layout';
 import { useEffect } from 'react';
+import { NavbarProps } from '@/components/navbar/navbar';
 
-const Sort = () => {
+export async function getStaticProps() {
+  const isSortEnabled = process.env.NAVBAR_SORT_ENABLED;
+  const isPathfinderEnabled = process.env.NAVBAR_PATHFIND_ENABLED;
+  const isTicTacToeEnabled = process.env.NAVBAR_TICTACTOE_ENABLED;
+
+  return {
+    props: {
+      navbarProps: {
+        enabledRoutes: {
+          isSortEnabled: isSortEnabled,
+          isPathfinderEnabled: isPathfinderEnabled,
+          isTicTacToeEnabled: isTicTacToeEnabled,
+        }
+      }
+    },
+  };
+}
+
+type SortProps = {
+  navbarProps: NavbarProps,
+}
+
+const Sort = ({navbarProps}: SortProps) => {
     useEffect(() => {
         document.title = "Sorter | Rang Corp";
     }, []);
 
     return (
-        <Layout>
+        <Layout navbarProps={navbarProps}>
             <Sorter />
         </Layout>
     )
